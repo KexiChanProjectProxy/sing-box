82ce318c87d3bc86cb5b44d4a80f0bb9d048faf4
kexichan <183220149+kexichan@users.noreply.github.com>
2026-07-03 11:08:45 -0400

    test(firefox-vpn): move TCP-only guardrail tests to integration package

--- test/firefoxvpn/outbound_test.go
+++ test/firefoxvpn/outbound_test.go
@@ -8,6 +8,7 @@ import (
 	"github.com/sagernet/sing-box/adapter"
 	"github.com/sagernet/sing-box/log"
 	"github.com/sagernet/sing-box/option"
+	protocolfirefoxvpn "github.com/sagernet/sing-box/protocol/firefoxvpn"
 	M "github.com/sagernet/sing/common/metadata"
 	N "github.com/sagernet/sing/common/network"
 	"github.com/sagernet/sing/service"
@@ -18,7 +19,7 @@ func TestFirefoxVPNListenPacketRejected(t *testing.T) {
 	t.Parallel()
 
 	ob := newTestOutbound(t)
-	_, err := ob.(*Outbound).ListenPacket(context.Background(), M.Socksaddr{})
+	_, err := ob.(*protocolfirefoxvpn.Outbound).ListenPacket(context.Background(), M.Socksaddr{})
 	require.ErrorIs(t, err, os.ErrInvalid)
 }
 
@@ -31,7 +32,7 @@ func TestFirefoxVPNDialContextRejectsNonTCP(t *testing.T) {
 			t.Parallel()
 
 			ob := newTestOutbound(t)
-			_, err := ob.(*Outbound).DialContext(context.Background(), network, M.Socksaddr{})
+			_, err := ob.(*protocolfirefoxvpn.Outbound).DialContext(context.Background(), network, M.Socksaddr{})
 			require.ErrorIs(t, err, N.ErrUnknownNetwork)
 		})
 	}
@@ -43,7 +44,7 @@ func newTestOutbound(t *testing.T) adapter.Outbound {
 	t.Helper()
 
 	ctx := service.ContextWith[adapter.OutboundManager](context.Background(), &stubOutboundManager{})
-	raw, err := NewOutbound(ctx, nil, log.NewNOPFactory().Logger(), "firefox-vpn", option.FirefoxVPNOutboundOptions{
+	raw, err := protocolfirefoxvpn.NewOutbound(ctx, nil, log.NewNOPFactory().Logger(), "firefox-vpn", option.FirefoxVPNOutboundOptions{
 		DialerOptions: option.DialerOptions{Detour: "direct"},
 		ServerOptions: option.ServerOptions{
 			Server:     "vpn.example.test",
@@ -58,18 +59,4 @@ func newTestOutbound(t *testing.T) adapter.Outbound {
 	})
 	require.NoError(t, err)
 	return raw
-}
-
-// stubOutboundManager is a minimal adapter.OutboundManager for tests that
-// need a context with an OutboundManager but do not actually start outbounds.
-type stubOutboundManager struct{}
-
-func (m *stubOutboundManager) Start(_ adapter.StartStage) error           { return nil }
-func (m *stubOutboundManager) Close() error                               { return nil }
-func (m *stubOutboundManager) Outbounds() []adapter.Outbound              { return nil }
-func (m *stubOutboundManager) Outbound(_ string) (adapter.Outbound, bool) { return nil, false }
-func (m *stubOutboundManager) Default() adapter.Outbound                  { return nil }
-func (m *stubOutboundManager) Remove(_ string) error                      { return nil }
-func (m *stubOutboundManager) Create(_ context.Context, _ adapter.Router, _ log.StructuredLogger, _ string, _ string, _ any) error {
-	return nil
 }
